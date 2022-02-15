4345cd17df81d62f043cb992b9cd6912c16af208
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-02-15 18:18:11 +0900

    Create 2558_A+B-2.cpp

---
+++ Algorithm/2558_A+B-2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    long long a, b;
+    cin >> a >> b;
+    cout << a + b;
+}
