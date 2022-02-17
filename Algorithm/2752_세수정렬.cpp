ac5b176c8a9372f9c4af61606d5d95b1e27b31bd
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-02-17 15:57:53 +0900

    Create 2752_세수정렬.cpp

---
+++ Algorithm/2752_세수정렬.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    vector <int> v(3);
+    cin >> v[0] >> v[1] >> v[2];
+    sort(v.begin(), v.end());
+    cout << v[0]  << " " << v[1] << " " << v[2];
+ }
