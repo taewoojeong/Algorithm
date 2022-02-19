a9e943a1efd8439443e3c22d6ec84082b9f9db96
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-02-19 16:36:57 +0900

    Create 2741_N찍기.cpp

---
+++ Algorithm/2741_N찍기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int N;
+    cin >> N;
+    for(int i = 1; i < N + 1; i++){
+        cout << i << "\n";
+    }
+    return 0;
+}
