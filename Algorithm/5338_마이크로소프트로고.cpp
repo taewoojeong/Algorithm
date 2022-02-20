5292d6e77bf93777b135ef28b028e0f7d33d67fd
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-02-21 00:13:19 +0900

    Create 5338_마이크로소프트로고.cpp

---
+++ Algorithm/5338_마이크로소프트로고.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int dp[1001][1001] = {0};
+
+int main(int argc, const char * argv[]){
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    cout << "       _.-;;-._\n'-..-'|   ||   |\n'-..-'|_.-;;-._|\n'-..-'|   ||   |\n'-..-'|_.-''-._|";
+}
