0acb82b606ca02f0aadc1b9fef377c0b3940dfae
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-02-20 13:10:25 +0900

    Create 2742_기찍N.cpp

---
+++ Algorithm/2742_기찍N.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int N;
+    cin >> N;
+    for(int i = N; i > 0; i--){
+        cout << i << "\n";
+    }
+    return 0;
+}
