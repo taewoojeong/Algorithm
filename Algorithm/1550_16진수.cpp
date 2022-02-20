5fc10c328ff81117848559022ef9aa83bb4967df
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-02-21 00:21:25 +0900

    Create 1550_16진수.cpp

---
+++ Algorithm/1550_16진수.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]){
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    string str;
+    cin >> str;
+    cout << stoi(str, NULL, 16);
+}
