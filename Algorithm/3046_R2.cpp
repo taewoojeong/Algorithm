9318a4a8d81ef25b7e5f890c4a8712f375a18203
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-02-22 00:23:34 +0900

    Create 3046_R2.cpp

---
+++ Algorithm/3046_R2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]){
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int R1, S;
+    cin >> R1 >> S;
+    cout << 2 * S - R1;
+}
