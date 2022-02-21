4259fe7573511cc7ac1413164e0aff716e0e19ab
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-02-22 00:17:00 +0900

    Create 3003_킹퀸룩비숏나이트폰.cpp

---
+++ Algorithm/3003_킹퀸룩비숍나이트폰.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int dp[1001][1001] = {0};
+
+int main(int argc, const char * argv[]){
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e >> f;
+    cout << 1 - a << " " << 1 - b << " " << 2 - c << " " << 2 - d << " " << 2 - e << " " << 8 - f;
+}
