6f6aeafba833e9afa9a605c4a0a77228951a046e
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-02-25 00:44:29 +0900

    Create 10926_??!.cpp

---
+++ Algorithm/10926_??!.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    string str;
+    cin >> str;
+    cout << str << "??!\n";
+    return 0;
+}
