7450ecac4d953c09801a0fcb34d40728f4f92ae4
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-02-25 00:40:05 +0900

    Create 10718_WeLoveKriii.cpp

---
+++ Algorithm/10718_WeLoveKriii.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    cout <<"강한친구 대한육군\n강한친구 대한육군";
+    return 0;
+}
