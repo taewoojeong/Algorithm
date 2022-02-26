c1bda277da0e678bdac3c4cf32627e5bb29cb72b
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-02-26 14:27:45 +0900

    Create 14645_와이버스부릉부릉.cpp

---
+++ Algorithm/14645_와이버스부릉부릉.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int N, K;
+    cin >> N >> K;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < K; j++){
+            int num;
+            cin >> num;
+        }
+    }
+    cout << "비와이";
+}
