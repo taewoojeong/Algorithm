8a7747c39763294752332e354f96139b4f640c74
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-02-26 14:26:19 +0900

    Create 11654_아스키코드.cpp

---
+++ Algorithm/11654_아스키코드.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    char c;
+    cin >> c;
+    cout << (int)c;
+    return 0;
+}
