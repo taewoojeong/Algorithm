4fc9b9b414ee7c87d939d5a405a3bcef05ef44cc
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-02-27 15:10:25 +0900

    Create 11382_꼬마정민.cpp

---
+++ Algorithm/11382_꼬마정민.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    long long A, B, C;
+    cin >> A >> B >> C;
+    cout << A + B + C;
+}
