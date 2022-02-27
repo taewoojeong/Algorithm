f97eea2390c8cfa746dbd876082fcca40ec692f7
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-02-27 15:13:32 +0900

    Create 15894_수학은체육과목입니다.cpp

---
+++ Algorithm/15894_수학은체육과목입니다.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    long long n;
+    cin >> n;
+    cout << n * 4;
+}
