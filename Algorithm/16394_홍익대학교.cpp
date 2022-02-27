21c31ad0f4e9906649f4511e5f1419244f059f22
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-02-28 00:14:54 +0900

    Create 16394_홍익대학교.cpp

---
+++ Algorithm/16394_홍익대학교.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int year;
+    cin >> year;
+    cout << year - 1946;
+}
