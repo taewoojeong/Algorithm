7a962fce6ab0a43499ef5539179d90b287568c24
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-03-01 14:02:46 +0900

    Create 17295_엔드게임스포일러.cpp

---
+++ Algorithm/17295_엔드게임스포일러.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    string str;
+    cin >> str;
+    cout << "Avengers: Endgame";
+}
