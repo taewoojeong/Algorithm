de0c69701542bd8e46747ae3a5e9960f506caea6
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-03-02 22:24:05 +0900

    Create 20492_세금.cpp

---
+++ Algorithm/20492_세금.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int N;
+    cin >> N;
+    cout << (int)(N * 0.78) << " " << (int)(N * 0.956);
+}
