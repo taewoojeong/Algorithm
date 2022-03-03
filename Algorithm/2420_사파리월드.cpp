c5ebc426c49a3a16d9edcbbecde6326c0e840517
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-03-03 15:24:33 +0900

    Create 2420_사파리월드.cpp

---
+++ Algorithm/2420_사파리월드.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main(int argc, const char * argv[]){
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    long long N, M;
+    cin >> N >> M;
+    cout << abs(N - M);
+}
