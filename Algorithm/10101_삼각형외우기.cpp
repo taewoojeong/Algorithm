3ae114c5092d7868e81193b393f990341fbc54af
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-03-05 15:08:04 +0900

    Create 10101_삼각형외우기.cpp

---
+++ Algorithm/10101_삼각형외우기.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]){
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a + b + c != 180){
+        cout << "Error";
+    }
+    else if(a == b && b == c){
+        cout << "Equilateral";
+    }
+    else if(a == b || b == c || a == c){
+        cout << "Isosceles";
+    }
+    else{
+        cout << "Scalene";
+    }
+}
