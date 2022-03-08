1100545b433453095525763c5c67a5492fcf54fc
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-03-08 21:34:33 +0900

    Create 15680_연세대학교.cpp

---
+++ Algorithm/15680_연세대학교.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int N;
+    cin >> N;
+    N ? cout << "Leading the Way to the Future" : cout << "YONSEI";
+}
