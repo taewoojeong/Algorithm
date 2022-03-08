63da66f32891dc19392b1a06f196bbf521b3d6b5
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-03-08 21:36:56 +0900

    Create 15700_타일채우기4.cpp

---
+++ Algorithm/15700_타일채우기4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    long long N, M;
+    cin >> N >> M;
+    cout << N * M / 2 << "\n";
+}
