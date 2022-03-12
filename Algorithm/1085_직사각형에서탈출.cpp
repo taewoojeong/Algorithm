568118048cb8916aafda85b1de2a03c179f5d034
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-03-12 21:59:00 +0900

    1085_직사각형에서탈출.cpp

---
+++ Algorithm/1085_직사각형에서탈출.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    vector <int> v(4);
+    for(int i = 0; i < 4; i++){
+        cin >> v[i];
+    }
+    v[2] -= v[0];
+    v[3] -= v[1];
+    sort(v.begin(), v.end());
+    cout << v[0];
+}
