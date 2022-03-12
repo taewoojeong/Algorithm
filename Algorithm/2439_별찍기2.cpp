d5285175c4c896f738f44bc4d93f73629c11a0b5
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-03-12 22:05:08 +0900

    Create 2439_별찍기2.cpp

---
+++ Algorithm/2439_별찍기2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    ios::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
+    int N;
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N - i - 1; j++){
+            cout << " ";
+        }
+        for(int j = 0; j < i + 1; j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
