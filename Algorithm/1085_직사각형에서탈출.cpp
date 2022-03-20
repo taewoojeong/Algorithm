4e98c7085ba207deeb3cde44d12cbe4c331d99ac
정태우 <97501702+taewoojeong@users.noreply.github.com>
2022-03-20 23:09:58 +0900

    Create 1085_직사각형에서탈출.cpp

--- Algorithm/1085_직사각형에서탈출.cpp
+++ Algorithm/1085_직사각형에서탈출.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     vector <int> v(4);
-    for(int i = 0; i < 4; i++){
+    for(int i = 0; i < 4; i++){ 
         cin >> v[i];
     }
     v[2] -= v[0];
