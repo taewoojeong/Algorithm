f9e404a1fbf766ec88aa4eca8f695effd8bb8d91
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-03-20 23:06:25 +0900

    Create 2460_지능형기차2.cpp

---
+++ Algorithm/2460_지능형기차2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int A, B;
+vector <int> v(11);
+int main(int argc, char *argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    for(int i = 1; i < 11; i++){
+        cin >> A >> B;
+        v[i] = B - A + v[i - 1];
+    }
+    sort(v.begin(), v.end());
+    cout << v[10] << "\n";
+}
