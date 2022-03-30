df791257a833971a2632b79d3f5fdb99c037ddd1
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-03-30 18:54:44 +0900

    Create 15651_N과M3.cpp

---
+++ Algorithm/15651_N과M3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int N, M, visited[10], ans[10];
+
+void permutation(int index){
+    if(index == M){
+        for(int i = 0; i < M; i++){
+            cout << ans[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for(int i = 1; i <= N; i++){
+        if(!visited[i]){
+            ans[index] = i;
+            permutation(index + 1);
+            ans[index] = 0;
+        }
+    }
+    return;
+}
+
+int main(int argc, const char * argv[]) {
+    cin >> N >> M;
+    permutation(0);
+}
