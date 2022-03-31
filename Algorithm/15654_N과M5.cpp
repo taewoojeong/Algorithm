72ff3e402e18bcaed4c8ec98d05c458a240b07f3
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-03-31 20:55:54 +0900

    Create 15654_N과M5.cpp

---
+++ Algorithm/15654_N과M5.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M, visited[10], ans[10], num;
+vector <int> v;
+
+void permutation(int index){
+    if(index == M){
+        for(int i = 0; i < M; i++){
+            cout << ans[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for(int i = 0; i < N; i++){
+        if(!visited[i]){
+            visited[i] = true;
+            ans[index] = v[i];
+            permutation(index + 1);
+            visited[i] = false;
+            ans[index] = 0;
+        }
+    }
+    return;
+}
+
+int main(int argc, const char * argv[]) {
+    cin >> N >> M;
+    for(int i = 0; i < N; i++){
+        cin >> num;
+        v.push_back(num);
+    }
+    sort(v.begin(), v.end());
+    permutation(0);
+}
