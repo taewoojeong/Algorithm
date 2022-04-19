b64e485b05c184cb219cb4eb1c333cddf78011b3
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-04-20 00:11:23 +0900

    2164_카드2.cpp

---
+++ Algorithm/2164_카드2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    ios::sync_with_stdio(false);cin.tie(NULL);
+    int N;
+    cin >> N;
+    queue <int> q;
+    for(int i = 1; i < N + 1; i++){
+        q.push(i);
+    }
+    while(q.size() > 1){
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+    cout << q.front();
+    return 0;
+}
