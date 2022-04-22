846a273d411b9d6667b42d58c6dc5027599e8a8c
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-04-22 21:57:38 +0900

    Create 2343_기타레슨.cpp

---
+++ Algorithm/2343_기타레슨.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+int N, M;
+int time_num[100005];
+
+int binary_search(int left, int right){
+    while(left <= right){
+        int blulay = 1, sum = 0;
+        int mid = (left + right) / 2;
+        for(int i = 0; i < N; i++){
+            if(time_num[i] + sum > mid){
+                blulay++;
+                sum = 0;
+            }
+            sum += time_num[i];
+        }
+        
+        if(blulay <= M)
+            right = mid - 1;
+        else
+            left = mid + 1;
+    }
+    return left;
+}
+
+int main(int argc, char *argv[]){
+    cin >> N >> M;
+    int total = 0, max = 0;
+    for(int i = 0; i < N; i++){
+        cin >> time_num[i];
+        total += time_num[i];
+        if(time_num[i] > max){
+            max = time_num[i];
+        }
+    }
+    cout << binary_search(max, total);
+    return 0;
+}
